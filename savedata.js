e7237c8dc0f34bf66d7755f6258d6dfed1a6e47f
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-06 10:02:09 -0300

    test!

--- savedata.js
+++ savedata.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <script>
  function enviar(){
     var nome = document.getElementById("txtnome").value;
@@ -10,3 +11,16 @@
                  
     }
  </script>
+=======
+function enviar() {
+    var nome = document.getElementById("txtnome").value;
+    var data = document.getElementById("date").value;
+    var cpf = document.getElementById("cpf").value;
+    var email = document.getElementById("email").value;
+
+    var texto = "Nome: " + nome + ";/n Data de Nascimento: " + data + "; /n CPF: " +
+        cpf + "; Email: " + email;
+    console.log(texto);
+    return true;
+}
+>>>>>>> Stashed changes
