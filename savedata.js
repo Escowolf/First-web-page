4e7ed33fb5f0e99886354ba6399da7c7e71cb037
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-06 10:05:30 -0300

    Update savedata.js

--- savedata.js
+++ savedata.js
@@ -1,12 +1,11 @@
-< script >
-    function enviar() {
-        var nome = document.getElementById("txtnome").value;
-        var telefone = document.getElementById("txttelefone").value;
-        var email = document.getElementById("txtemail").value;
+function enviar() {
+    var nome = document.getElementById("txtnome").value;
+    var data = document.getElementById("date").value;
+    var cpf = document.getElementById("cpf").value;
+    var email = document.getElementById("email").value;
 
-        var texto = "Nome: " + nome + "; Telefone: " + telefone + "; Email: " + email;
-
-        return true;
-
-    } <
-    /script>
+    var texto = "Nome: " + nome + ";/n Data de Nascimento: " + data + "; /n CPF: " +
+        cpf + "; Email: " + email;
+    console.log(texto);
+    return true;
+}
