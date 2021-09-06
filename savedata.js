fcce5940def3caf3c76bef0710b361a2054cc86f
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-06 10:04:05 -0300

    test!

--- savedata.js
+++ savedata.js
@@ -1,26 +1,12 @@
-<<<<<<< Updated upstream
-<script>
- function enviar(){
-    var nome = document.getElementById("txtnome").value;
-    var telefone = document.getElementById("txttelefone").value;
-    var email = document.getElementById("txtemail").value;
-                 
-    var texto = "Nome: " +nome + "; Telefone: "+ telefone + "; Email: "+email;
-                 
-    return true;
-                 
-    }
- </script>
-=======
-function enviar() {
-    var nome = document.getElementById("txtnome").value;
-    var data = document.getElementById("date").value;
-    var cpf = document.getElementById("cpf").value;
-    var email = document.getElementById("email").value;
+< script >
+    function enviar() {
+        var nome = document.getElementById("txtnome").value;
+        var telefone = document.getElementById("txttelefone").value;
+        var email = document.getElementById("txtemail").value;
 
-    var texto = "Nome: " + nome + ";/n Data de Nascimento: " + data + "; /n CPF: " +
-        cpf + "; Email: " + email;
-    console.log(texto);
-    return true;
-}
->>>>>>> Stashed changes
+        var texto = "Nome: " + nome + "; Telefone: " + telefone + "; Email: " + email;
+
+        return true;
+
+    } <
+    /script>
