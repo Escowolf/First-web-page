b984ce7238662212c0b5c5eaee90c462847665f8
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-08 19:58:35 -0300

    estrutura html
    criação de header, footer, js e modificações no css.

--- savedata.js
+++ savedata.js
@@ -1,10 +1,41 @@
-function enviar(){
-    var nome = document.getElementById("txtnome").value;
-    var data = document.getElementById("date").value;
-    var cpf = document.getElementById("cpf").value;
-    var email = document.getElementById("email").value;
-                 
-    var texto = "Nome: " +nome + "; Data de Nascimento: "+ data + "; Email: "+email;
-      console.log(texto);            
+function TestaCPF(cpf) {
+    var Soma;
+    var Resto;
+
+    Soma = 0;
+    if (cpf == "00000000000") {
+        return false;
+    }
+
+    for (i = 1; i <= 9; i++) {
+        Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+    }
+
+    if ((Resto == 10) || (Resto == 11)) {
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) {
+        Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+    }
+
+
+    if ((Resto == 10) || (Resto == 11)) {
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
     return true;
-   }
+}
+let cpf = "0123456789";
+console.log(TestaCPF(cpf));
