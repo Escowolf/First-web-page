27c9f348d189428a52b33c7590d6d2cd8b4ddc3a
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-13 19:03:29 -0300

    update js

--- js/checkData.js
+++ js/checkData.js
@@ -1,12 +1,9 @@
-<script language="javascript" type="text/javascript">
 function validar() {
-  var nome = form1.nome.value;
-  var email = form1.email.value;
-  
-  if (nome == "") {
-    alert('Preencha o campo com seu nome');
-    form1.nome.focus();
-    return false;
-  }
+    var nome = form.name.value;
+
+    if (nome == "") {
+        alert('Preencha o campo com seu nome');
+        form.name.focus();
+        return false;
+    }
 }
-</script>
