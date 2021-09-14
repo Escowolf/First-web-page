1b123a54966db3ccbf19d323bd3b3319d378cfa9
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-09-13 21:05:56 -0300

    Update checkData.js

--- js/checkData.js
+++ js/checkData.js
@@ -1,4 +1,9 @@
 function checkData(name, cpf, email, date) {
+    var name = document.getElementById(name);
+    var date = document.getElementById(date);
+    var cpf = document.getElementById(cpf);
+    var email = document.getElementById(email);
+
     alert("Nome: " + nome + "\n Data de nascimento: " + date + "\n CPF: " + cpf + "\n E-mail: " + email);
     console.log()
 }
